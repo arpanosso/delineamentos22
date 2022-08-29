f0f5385e440a0a1cb7d9b8cfa15090d0303ad56b
arpanosso <arpanosso@yahoo.com.br>
2022-08-29 19:16:24 -0300

    aula02

---
+++ aula02/scriipt02.R
@@ -0,0 +1,30 @@
+## 1) Instalar os pacotes necessários para realizar o exercício.
+## São eles, "ExpDes.pt", "agricolae" "tidyver", "ggpubr" e "Deriv".
+
+## 2) Carregar os pacotes.
+
+## 3) Realizar a entra de dados, pasta data nome aula2.xlsx
+
+## 4) Calcular a média geral do experimento
+
+## 5) Calcula a soma de quadrados totais, apenas para praticar.
+
+## 6) Construir o gráfico de dispersão
+
+## 7) Calcular a Diferença, ou os Desvios para o modelo de médias
+
+## 8) Construir o quadro da análise de variância
+
+## 9) Utilize o pacote ExpDes.pt
+
+## 10) Realizar a análise de regressão linear
+
+## 11) Tabela com o modelo parabólico
+
+## 12) Adicionando a equação polinomial no gráfico de dispersão
+
+## 13) Utilziar a função Deriv para calcular o ponto de máximo
+
+## 14) Calcular a tabela com o Erro Puro
+
+## 15) Adicionando a equação linear no gráfico de dispersão
