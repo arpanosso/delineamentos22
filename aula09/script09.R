80556632b5779d1d9cce0aef4ba822629c6f7b74
arpanosso <arpanosso@yahoo.com.br>
2022-11-05 09:49:20 -0300

    script 09

--- aula09/script09.R
+++ aula09/script09.R
@@ -9,6 +9,12 @@ fat333 <- readxl::read_xlsx("data/aula9.xlsx",
                            sheet = "Fat333")
 glimpse(fat333)
 
+mod <- aov(RESP ~ BL + A*B*C,
+   data = fat333 %>%
+     mutate_at(vars(A,B,C,BL), as.factor)
+)
+anova(mod)
+
 # lendo
 tres_fat <- readxl::read_xlsx("data/aula9.xlsx",
                             sheet = "TresFatresSas")
@@ -18,3 +24,10 @@ glimpse(tres_fat)
 metade_25 <- readxl::read_xlsx("data/aula9.xlsx",
                               sheet = "Metades2_5")
 glimpse(metade_25)
+mod <- aov(RESP ~ BLOCO + A + B + C+ D+ E + A:B + A:C + A:D
+           + A:E + B:C + B:D + B:E + C:D + C:E + D:E,
+           data = metade_25 %>%
+             mutate_at(vars(A,B,C,D,E,BLOCO), as.factor)
+)
+anova(mod)
+
