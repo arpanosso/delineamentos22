0374ae48153b8a60a5bb143faf2d52d458ade06c
arpanosso <arpanosso@yahoo.com.br>
2022-11-09 13:49:46 -0300

    aula 09

--- aula09/script09.R
+++ aula09/script09.R
@@ -2,86 +2,55 @@
 library(tidyverse)
 library(skimr)
 library(lme4)
+library(readxl)
+library(VCA)
 
 # 1) Utilizando a função "excel_sheets" do pacote {readxl} extraia
-# os nomes das planilhas do arquivo aula9.xlsx
-pla <-readxl::excel_sheets("data/aula9.xlsx")
+# os nomes das planilhas do arquivo aula9.xlsx, da pasta data
 
 # COM CONFUNDIMENTO, BLOCOS DE TAMANHO 9 -----------------------------------
-# Ler o banco de dados
-fat333 <- readxl::read_xlsx("data/aula9.xlsx",
-                           sheet = pla[1])
-# vislumbre
-glimpse(fat333)
+# Leia o banco de dados da planilha "Fat3_confundimento"
 
-# resumo rápido
-skim(fat333)
+# Construa um resumo simples do banco de dados, utilize glimpse e skim
 
-# Criação do modelo
-mod <- aov(RESP ~ BL + A*B*C,
-   data = fat333 %>%
-     mutate_at(vars(A,B,C,BL), as.factor)
-)
+# Realizar a análise de variância com os fatores A, B, C e BL
+# definição do modelo
 
 # Análise de variância
-anova(mod)
 
 # "EXEMPLO BLOCO metade de 2^5" --------------------------------------------
-# Ler o banco de dados
-metade_25 <- readxl::read_xlsx("data/aula9.xlsx",
-                              sheet = pla[2])
-# Vislumbre dos dados
-glimpse(metade_25)
+# Ler o banco de dados da planilha "Metades2a5"
 
-# Criação do modelo
-mod <- aov(RESP ~ BLOCO + A + B + C+ D+ E + A:B + A:C + A:D
-           + A:E + B:C + B:D + B:E + C:D + C:E + D:E,
-           data = metade_25 %>%
-             mutate_at(vars(A,B,C,D,E,BLOCO), as.factor)
-)
+# Vislumbre dos dados
+
+# Realizar a análise de variância com os fatores A, B, C, D, E,
+# e a interações duplas e BL
+# definição do modelo
 
 # Análise de variância
-anova(mod)
 
-# 'ATORIAL 3 FATORES - modelos mistos --------------------------------------
+# FATORIAL 3 FATORES - modelos mistos --------------------------------------
 # lendo
 tres_fat <- readxl::read_xlsx("data/aula9.xlsx",
-                              sheet = pla[3])
+                              sheet = "Fat3_misto")
 glimpse(tres_fat)
 
 ## Análise preliminar com os tratamentos
 
-mod <- aov(Resp ~ Trat,
-           data = tres_fat %>% mutate_at(vars(Trat), as_factor))
-
-anova(mod)
-
 ## Análise com 3 fatores
-
 mod <- aov(terms(Resp ~ A+ B +A:B +C + A:C + B:C + A:B:C),
            data = tres_fat %>% mutate_at(vars(A,B,C), as_factor))
-
 anova(mod)
-
-car::Anova(mod, type=2)
-
 mod <- lme4::lmer(Resp ~ A + (1 | B) + (1 | C)+
-                    (1 | A:B) + (1 | A:C) + (1 | B:C)+
-                    (1 | A:B:C),
-           data = tres_fat %>%
-             mutate_at(vars(A,B,C), as_factor)
+                    (1 | A:B) + (1 | A:C) + (1 | B:C),
+                  data = tres_fat %>%
+                    mutate_at(vars(A,B,C), as_factor)
 )
 summary(mod)
-VCA::fitVCA(Resp ~ A*B*C, tres_fat %>% data.frame() )
-as.data.frame(ranef(mod))
-confint(mod)
+fitVCA(Resp ~ A*B*C, tres_fat %>% data.frame() )
 VarCorr(mod) # Variância
-
-
-data.frame(ranef(mod))
-
-
+# data.frame(ranef(mod))
 emm <- emmeans::emmeans(mod, specs = ~A)
-
 pairs(emm,
       adjust="tukey")
+
