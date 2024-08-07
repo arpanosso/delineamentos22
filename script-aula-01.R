3efca688d592b91f718a48b68d5fbb55172c7631
arpanosso <arpanosso@yahoo.com.br>
2024-08-07 20:53:01 -0300

    first 2024

---
+++ script-aula-01.R
@@ -0,0 +1,39 @@
+# Aula 01 - Dia 07/08/2024 - Alan R Panosso
+# O R como caluladora
+
+# Operadores Básicos
+2+2
+2-2
+2*2
+2/2
+5%/%2
+5%%2
+
+# Funções Matemáticas e Trigonométricas
+sqrt(1/log(100,2))
+
+# Funções estatísticas
+mean(c(21, 22, 43))
+
+# exemplo de cálculo de área
+curve(dnorm(x,5,0.8), 0, 8)
+abline(v=5, col="red")
+abline(v=2)
+abline(v=3)
+pnorm(3,5,0.8) - pnorm(2,5,0.8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
